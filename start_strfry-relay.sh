6a676c6805762cde11badd396fa623c82c5039b6
fred <support@qo-op.com>
2025-06-08 16:31:26 +0200

    star new and kill old

--- start_strfry-relay.sh
+++ start_strfry-relay.sh
@@ -2,19 +2,12 @@
 ## THIS SCRIPT IS LAUNCHING "strfry relay"
 
 if [[ ! -x $HOME/.zen/strfry/strfry ]]; then
-    echo "$HOME/.zen/strfry/strfry is missing !! "
-    exit 1
+echo "$HOME/.zen/strfry/strfry is missing !! "
+exit 1
 fi
 
 ## CHECK IF ALREADY RUNNING
-OLD_PID=$(cat $HOME/.zen/strfry/.pid 2>/dev/null)
-
-## KILL OLD RELAY FIRST
-if [[ ! -z $OLD_PID ]]; then
-    echo "Stopping old strfry PID : $OLD_PID"
-    kill -USR1 $OLD_PID 2>/dev/null
-    sleep 1
-fi
+OLD_PID=$(cat $HOME/.zen/strfry/.pid)
 
 ## LAUNCHING NEW RELAY
 cd $HOME/.zen/strfry/
@@ -22,6 +15,13 @@ cd $HOME/.zen/strfry/
 
 ./strfry relay &
 echo $! > .pid
-cd - >/dev/null 2>&1
+cd -
+
+## KILL OLD REALY
+if [[ ! -z $OLD_PID ]]; then
+echo "Stopping old strfry PID : $OLD_PID"
+    kill $OLD_PID
+    kill -USR1 $OLD_PID
+fi
 
 exit 0
